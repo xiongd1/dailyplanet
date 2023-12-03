fe4cba28a9c15e88d9b758aabd0a270307c7734a
Jesse Xiong <xiongd15@hotmail.com>
2023-12-02 23:23:41 -0500

    more updates

--- paper.js
+++ paper.js
@@ -28,22 +28,21 @@ for (var i = 0; i < enemies.length; i++) {
     console.log(whoWins(isThereKryptonite, enemies[i]));
 }
 
-function howAttractedIsLoisLaneToMe()
-{
+function howAttractedIsLoisLaneToMe() {
     // 1 is not at all attracted, 10 is "super" attracted...
     return Math.floor((Math.random()*10)+1);
 }
 
-console.log(howAttractedIsLoisLaneToMe()) ;
+console.log(howAttractedIsLoisLaneToMe());
 
 var clarkKent = true;
 var superman = false;
 
-while (clarkKent){
+while (clarkKent) {
     console.log("I'm just a nerdy columnist");
 
 var phoneBoothQuickChange = Math.random();
-    if (phoneBoothQuickChange >= 0.5){
+    if (phoneBoothQuickChange >= 0.5) {
         clarkKent = false;
         superman = true;
         console.log("Now I'm Superman!");
